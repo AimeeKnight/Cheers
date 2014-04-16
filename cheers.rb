5ca2f40ca8eda60c3c5d8f3999aa19234e3424e0
Aimee <AimeeKnight83@gmail.com>
2014-04-16 15:52:26 -0500

    test and implement guards for blank names, number names and names ending in with 's'

--- cheers.rb
+++ cheers.rb
@@ -1,20 +1,27 @@
-# my_array = ["foo", "bar", "grille"]
+vowels = ["A", "E", "I", "O", "U"]
 
-# my_array.each do |item|
-  # puts item.capitalize
-# end
-
-vowels = ["a", "e", "i", "o", "u"]
 puts "What's your name?"
-name = gets.chomp.downcase
+name = gets.chomp.upcase
+name_length = name.length
+
+while name.empty? or name.to_i.to_s == name
+  puts "Please enter your name (it shouldn't be blank or have numbers)"
+  puts "What's your name?"
+  name = gets.chomp.upcase
+end
 
 name.each_char do |character|
   if vowels.include? character
-    puts "Give me an... #{character.capitalize}"
+    puts "Give me an... #{character}"
   else
-    puts "Give me a... #{character.capitalize}"
+    puts "Give me a... #{character}"
   end
 end
-puts "#{name.upcase}'s just GRAND!"
+
+if name[name_length -1] == "S"
+  puts "#{name} is just GRAND!"
+else
+  puts "#{name}'s just GRAND!"
+end
 
 
--- test/test_cheers_integration.rb
+++ test/test_cheers_integration.rb
@@ -5,16 +5,100 @@ class TestCheersIntegration < MiniTest::Unit::TestCase
   def test_a_name_with_no_vowels
     shell_output = ""
     IO.popen('ruby cheers.rb', 'r+') do |pipe|
-      pipe.puts "BRT"
+      pipe.puts "KC"
       pipe.close_write
       shell_output = pipe.read
     end
     expected_output = <<EOS
-Give me a.. B
-Give me a.. R
-Give me a.. T
+What's your name?
+Give me a... K
+Give me a... C
+KC's just GRAND!
 EOS
     assert_equal expected_output, shell_output
   end
 
+  def test_a_name_with_vowels
+    shell_output = ""
+    IO.popen('ruby cheers.rb', 'r+') do |pipe|
+      pipe.puts "Aimee"
+      pipe.close_write
+      shell_output = pipe.read
+    end
+    expected_output = <<EOS
+What's your name?
+Give me an... A
+Give me an... I
+Give me a... M
+Give me an... E
+Give me an... E
+AIMEE's just GRAND!
+EOS
+    assert_equal expected_output, shell_output
+  end
+
+  def test_keeps_asking_for_name_when_the_name_is_nil
+      shell_output = ""
+      IO.popen('ruby cheers.rb', 'r+') do |pipe|
+        pipe.puts ""
+        pipe.puts "Aimee"
+        pipe.close_write
+        shell_output = pipe.read
+      end
+      expected_output = <<EOS
+What's your name?
+Please enter your name (it shouldn't be blank or have numbers)
+What's your name?
+Give me an... A
+Give me an... I
+Give me a... M
+Give me an... E
+Give me an... E
+AIMEE's just GRAND!
+EOS
+    assert_equal expected_output, shell_output
+  end
+
+  def test_keeps_asking_for_name_when_the_name_is_a_number
+      shell_output = ""
+      IO.popen('ruby cheers.rb', 'r+') do |pipe|
+        pipe.puts ""
+        pipe.puts "Aimee"
+        pipe.close_write
+        shell_output = pipe.read
+      end
+      expected_output = <<EOS
+What's your name?
+Please enter your name (it shouldn't be blank or have numbers)
+What's your name?
+Give me an... A
+Give me an... I
+Give me a... M
+Give me an... E
+Give me an... E
+AIMEE's just GRAND!
+EOS
+    assert_equal expected_output, shell_output
+
+  end
+
+  def test_a_name_ending_in_an_s
+      shell_output = ""
+      IO.popen('ruby cheers.rb', 'r+') do |pipe|
+        pipe.puts "Aimers"
+        pipe.close_write
+        shell_output = pipe.read
+      end
+      expected_output = <<EOS
+What's your name?
+Give me an... A
+Give me an... I
+Give me a... M
+Give me an... E
+Give me a... R
+Give me a... S
+AIMERS is just GRAND!
+EOS
+    assert_equal expected_output, shell_output
+  end
 end
